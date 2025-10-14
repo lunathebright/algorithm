f94b6a78a6404a4a78bb8cf3404fce98b28541be
Luna <lunabright817@gmail.com>
2025-10-14 23:19:19 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.97 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,22 @@
-function solution(s) {
-    let numberOfZero = 0;
-    let numberOfConvert = 0;
+function solution(s) { 
+    let result = s;
+    let count = 0;
+    let zeros = 0;
     
-    while(s !== "1") {
-        const zero = (s.match(/0/g) || []).length;
-        numberOfZero += zero;
-        s = s.replaceAll("0", "").length.toString(2);
-        numberOfConvert++;
+    function getNewBinary (str) {
+        const removeZero = str.split("")
+                              .filter(num => num !== "0");
+        const removedZero = str.split("")
+                               .filter(num => num === "0");
+        zeros += removedZero.length
+        return removeZero.length.toString(2);
+    }
+    
+    
+    while(result !== "1") {
+        result = getNewBinary(result);
+        count++;
     }
 
-    return [numberOfConvert, numberOfZero]
+    return [count, zeros]
 }
