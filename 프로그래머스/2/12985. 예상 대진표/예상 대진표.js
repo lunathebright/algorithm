b4a5c36a822882f3bdc67221ca254204062ac4e5
Luna <lunabright817@gmail.com>
2025-10-15 18:41:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,12 @@
+function solution(n,a,b) {
+    let round = 1;
+    
+    while(true) {
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        
+        if(a === b) return round;
+        
+        round++
+    }
+}
